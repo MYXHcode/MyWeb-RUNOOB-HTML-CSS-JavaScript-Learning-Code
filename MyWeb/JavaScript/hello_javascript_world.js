3459ed7ecf4ab44c03c727ee1f996525eb210ae6
MYXH <1735350920@qq.com>
2023-08-18 16:39:28 +0800

    Initial commit

---
+++ MyWeb/JavaScript/hello_javascript_world.js
@@ -0,0 +1,4 @@
+function myFunction1()    //这是一个注释
+{
+    document.getElementById("demo_1").innerHTML = "Hello JavaScript World!";
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MyWeb-RUNOOB-HTML-CSS-JavaScript-Learning-Code
+MyWeb——RUNOOB HTML CSS javaScript 学习代码
